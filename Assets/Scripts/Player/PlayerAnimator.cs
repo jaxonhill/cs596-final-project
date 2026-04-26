511ea594dda741dc6a6cf4fc78bdd88a846f678b
Jaxon Hill <jaxoncharleshill@gmail.com>
2026-04-26 15:06:03 -0700

    Clean up player animation code, as null checks not needed

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
-[RequireComponent(typeof(CharacterController))]
 public class PlayerAnimator : MonoBehaviour
 {
     [Header("Required Components")]
@@ -16,34 +15,13 @@ public class PlayerAnimator : MonoBehaviour
 
     private int isMovingParameterHash;
 
-    private void Reset()
-    {
-        animator = GetComponent<Animator>();
-        characterController = GetComponentInParent<CharacterController>();
-    }
-
     private void Awake()
     {
-        if (animator == null)
-        {
-            animator = GetComponent<Animator>();
-        }
-
-        if (characterController == null)
-        {
-            characterController = GetComponentInParent<CharacterController>();
-        }
-
         isMovingParameterHash = Animator.StringToHash(isMovingParameter);
     }
 
     private void LateUpdate()
     {
-        if (animator == null || characterController == null)
-        {
-            return;
-        }
-
         Vector3 horizontalVelocity = characterController.velocity;
         horizontalVelocity.y = 0f;
 
